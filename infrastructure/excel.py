2625b96fc97ac6e0f9e6a5ec15dd0bb6a4821bfa
Ivan Panchenko <arigatory@gmail.com>
2023-12-29 23:14:13 +0300

    add colors

--- infrastructure/excel.py
+++ infrastructure/excel.py
@@ -1,62 +1,67 @@
-import uuid
+import operator
 import xlsxwriter
-from datetime import datetime
-from dataclasses import dataclass
-
-
-@dataclass
-class UserRecord:
-    id: uuid.UUID
-    telegramId: int
-    name: str
-    status: str
-    date: datetime
+import pprint
+from postgres import fetch_users_from_database
+import itertools
 
 
 # Sample data for UserRecord instances
-user_records = [
-    UserRecord(uuid.uuid4(), 123, 'John Doe', 'Active', datetime(2023, 1, 1)),
-    UserRecord(uuid.uuid4(), 456, 'Alice Smith',
-               'Inactive', datetime(2023, 1, 2)),
-    UserRecord(uuid.uuid4(), 789, 'Bob Johnson',
-               'Active', datetime(2023, 1, 1)),
-    UserRecord(uuid.uuid4(), 101, 'Eve Williams', '', datetime(2023, 1, 3)),
-    # Add more sample data as needed
-]
+user_records = fetch_users_from_database()
 
 # Extract unique dates and sort them
 unique_dates = sorted(set(record.date.date() for record in user_records))
 # Sort user records by name
-user_records.sort(key=lambda record: record.name)
+user_records = sorted(user_records, key=operator.attrgetter("name", "date"))
+
+
+table = {}
+for record in user_records:
+    if record.telegramId not in table:
+        table[record.telegramId] = {"telegramId": record.telegramId,
+                                    "dates": zip(unique_dates, itertools.repeat('Не ответил'))}
+    date = record.date.date()
+    table[record.telegramId][date] = record.status
+
+pprint.pprint(table)
+pprint.pprint(unique_dates)
+# pprint.pprint(user_records)
 
 workbook = xlsxwriter.Workbook("Records.xlsx")
 worksheet = workbook.add_worksheet()
 
 # Define a format for the blue cells
 blue_format = workbook.add_format(
-    {'bg_color': '#0000FF', 'font_color': '#FFFFFF'})
+    {'bg_color': '#62C6FF'})
+red_format = workbook.add_format(
+    {'bg_color': '#FF6262'})
+orange_format = workbook.add_format(
+    {'bg_color': '#FFB762'})
+green_format = workbook.add_format(
+    {'bg_color': '#62FF97'})
 
 
-# Write the header row with dates
-worksheet.write('A1', 'Name')
-for col_num, date in enumerate(unique_dates, start=1):
-    col_letter = chr(ord('A') + col_num)
-    worksheet.write(0, col_num, date.strftime('%Y-%m-%d'))
+for i in range(len(unique_dates)):
+    worksheet.write(0, i + 1, unique_dates[i].strftime('%d-%m-%Y'))
 
 # Write user data to the worksheet
-for row_num, user_record in enumerate(user_records, start=1):
-    worksheet.write(row_num, 0, user_record.name)
+for i, key in enumerate(table):
+    # key - имя
+    worksheet.write(i+1, 0, key)
 
-    for col_num, date in enumerate(unique_dates, start=1):
-        col_letter = chr(ord('A') + col_num)
-        status = next((record.status for record in user_records if record.date.date(
-        ) == date and record.name == user_record.name), 'NoData')
-
+    for j in range(len(unique_dates)):
+        status = table[key].get(unique_dates[j], "NoData")
+        
         # Apply conditional formatting to cells with 'NoData'
-        if status == 'NoData':
-            worksheet.write(row_num, col_num, status, blue_format)
+        if status == 'Не ответил':
+            worksheet.write(i+1, j+1, status, blue_format)
+        elif status == 'Болен':
+            worksheet.write(i+1, j+1, status, red_format)
+        elif status == 'Выздоравливает':
+            worksheet.write(i+1, j+1, status, orange_format)
+        elif status == 'Здоров':
+            worksheet.write(i+1, j+1, status, green_format)
         else:
-            worksheet.write(row_num, col_num, status)
+            worksheet.write(i+1, j+1, status)
 
 # Save the workbook and close it.
 workbook.close()
